b63700de324cf731217768915ec4b4bfa34bd0ea
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-09-29 23:01:22 +0000

    Added img card and updated bootstrap

--- js/sandbox.js
+++ js/sandbox.js
@@ -32,9 +32,9 @@ async function getWeather(apiUrl) {
     //objectTranslate(response.data);
     weatherState.city = response.data.name;
     weatherState.temperature.kelvin = Math.round(response.data.main.temp);
-    weatherState.temperature.fahr = Math.round((response.data.main.temp - 273.15) * 1.8 + 32) + ': F';
+    weatherState.temperature.fahr = Math.round((response.data.main.temp - 273.15) * 1.8 + 32) + ' F';
     weatherState.temperature.celcius = Math.round((response.data.main.temp -273.15)
-    ) + ': C';
+    ) + ' C';
     weatherState.condition = response.data.weather[0].main;
     weatherState.otherInfo = response.data.weather[0].icon;
     apiPagePop(createElements);              
@@ -51,15 +51,15 @@ async function getWeather(apiUrl) {
 //CREATING INIT ELEMENTS
 let master = document.getElementById('master');
 master.id = 'master';
-master.className = 'container';
+master.className = 'container text-center';
   
   
 function INIT () {
   //Callback function creates by element, parentId, qty, text, class, ID 
   createElements('div', 'master', 1,'', 'row header','headRow');
-  createElements('div', 'headRow', 1,'', 'col header','headCol1');
-  createElements('div', 'headRow', 1,'', 'col header','headCol2');
-  createElements('h1', 'headRow', 1,'Weather App', 'header', 'headerText');
+  createElements('div', 'headRow', 1,'Weather App', 'col-12','headColMain');
+  createElements('div', 'headRow', 1,'', 'col-4 offset-2','headCol1');
+  createElements('div', 'headRow', 1,'', 'col-4 offset-2','headCol2');
   createElements('input', 'headCol1', 1,'', 'headerZipEnt', 'zipInput');
   //inputZip.type = 'numeric';
   //inputZip.maxlength = '5';
@@ -70,21 +70,29 @@ function INIT () {
 };
 
 function apiPagePop (createElements) {
-  createElements('div', 'master', 1,'', 'row body','cityRow');
-  createElements('div', 'cityRow', 1,'City', 'col body','cityCol');
-  createElements('p', 'cityCol', 1, weatherState.city, 'col body','city');  
-  createElements('div', 'master', 1,'Temperature', 'row body','tempRow');
-  createElements('div', 'tempRow', 1,'', 'col body','kelvinCol');
+  createElements('div', 'master', 1,'', 'row  border border-success p-4','cityRow');
+  createElements('div', 'cityRow', 1,'City', 'col-12 bg-warning p-1','cityCol');
+  createElements('p', 'cityRow', 1, weatherState.city, 'col-12','city');  
+  createElements('div', 'master', 1,'', 'row border border-success p-4','tempRow');
+  createElements('div', 'tempRow', 1,'Temperature', 'col-12 bg-warning p-1','tempHead');  
+  createElements('div', 'tempRow', 1,'', 'col-4','kelvinCol');
   createElements('p', 'kelvinCol', 1, weatherState.temperature.kelvin, 'col body','kelvin'); 
-  createElements('div', 'tempRow', 1,'', 'col body','fahrCol');
+  createElements('div', 'tempRow', 1,'', 'col-4','fahrCol');
   createElements('p', 'fahrCol', 1, weatherState.temperature.fahr, 'col body','fahr');
-  createElements('div', 'tempRow', 1,'', 'col body','celciusCol');   
-  createElements('p', 'kelvinCol', 1, weatherState.temperature.celcius, 'col body','celcius');
-  createElements('div', 'master', 1,'Condition', 'row body','condiRow');
-  createElements('div', 'condiRow', 1,'', 'col body','condiCol');
+  createElements('div', 'tempRow', 1,'', 'col-4','celciusCol');   
+  createElements('p', 'celciusCol', 1, weatherState.temperature.celcius, 'col body','celcius');
+  createElements('div', 'master', 1,'', 'row border border-success p-4','condiRow');
+  createElements('div', 'condiRow', 1,'Condition', 'col-12 bg-warning p-1','condiHead');
+  createElements('div', 'condiRow', 1,'', 'col-12','condiCol');
   createElements('p', 'condiCol', 1, weatherState.condition, 'col body','condition');
-  createElements('div', 'master', 1,'Other Info', 'row body','imgRow');
-  createElements('div', 'imgRow', 1,'', 'col body','imgCol');
+  //createElements('div', 'master', 1,'', 'row border border-success p-4','imgRow');
+  //createElements('div', 'imgRow', 1,'Other Info', 'col-12 bg-warning p-1','imgCol');
+  createElements('div', 'master', 1,'', 'card','imgHead');
+  imgHead = document.getElementById('imgHead');
+  imgHead.style = "width: 18rem";
+  createElements('img', 'imgHead', 1,'', 'card-img-top','imgSrc');
+  imgSrc = document.getElementById('imgSrc');
+  imgSrc.src = `https://openweathermap.org/img/wn/${weatherState.otherInfo}@2x.png`;
 };
 
 window.addEventListener('DOMContentLoaded', (event) => {
